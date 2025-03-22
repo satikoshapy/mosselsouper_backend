c8cb05b6bd056d85ad673d061f83125b76ab3843
satikoshapy <satikoshapy@yahoo.com>
2025-03-22 16:56:09 +0100

    finished

--- src/main/java/be/pxl/mousselsouper_backend/MollieController.java
+++ src/main/java/be/pxl/mousselsouper_backend/MollieController.java
@@ -4,12 +4,13 @@ package be.pxl.mousselsouper_backend;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/payments")
 public class MollieController {
